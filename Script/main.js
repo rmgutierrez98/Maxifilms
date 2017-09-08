e6a4ff7e1c39aa879bf646518d2e7e9ad589cc29
rmgutierrez98 <rmgutierrez0@misena.edu.co>
2017-09-08 15:05:54 -0500

    deploy de la pagina

---
+++ Script/main.js
@@ -0,0 +1,20 @@
+$(document).ready(main);
+ 
+let contador = 1;
+ 
+function main () {
+	$('.menu_phone').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+ 
+}
